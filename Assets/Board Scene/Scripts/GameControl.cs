6ab52276f4f2730843dad4a4e3d1e9dff1ac1f05
Yunxin Chen <yuc036@users.noreply.github.com>
2021-09-26 23:04:25 -0700

    Add Mini-Game Hexgon

---
+++ Assets/Board Scene/Scripts/GameControl.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameControl : MonoBehaviour {
+
+    private static GameObject winsTextShadow, playerMoveCount;
+
+    private static GameObject player1;
+
+    private static GameObject sceneManager;
+
+    public static int diceSideThrown = 0;
+
+    public static bool gameOver = false;
+
+    private static bool isRewarded = true;
+
+    HashSet<int> minigamesIndexes = new HashSet<int>();
+    private int[] waypoints_reward = {0, 0, 0, 4, -1, 0, 0,
+                                      0, 0, 0, -10, 0, 0,
+                                      0, 0, 0, -1, 0, -2,
+                                      0, 0, 0, 0, 0};
+    // Use this for initialization
+    void Start () {
+        minigamesIndexes.Add(3);
+        minigamesIndexes.Add(4);
+        minigamesIndexes.Add(5);
+        minigamesIndexes.Add(6);
+        minigamesIndexes.Add(9);
+        minigamesIndexes.Add(10);
+        minigamesIndexes.Add(12);
+        minigamesIndexes.Add(13);
+        minigamesIndexes.Add(15);
+        minigamesIndexes.Add(16);
+        minigamesIndexes.Add(19);
+        minigamesIndexes.Add(21);
+        minigamesIndexes.Add(23);
+
+        winsTextShadow = GameObject.Find("WinsText");
+        playerMoveCount = GameObject.Find("Player1MoveCount");
+        player1 = GameObject.Find("Player1");
+        sceneManager = GameObject.Find("SceneManager");
+
+        player1.GetComponent<PlayerMovement>().moveAllowed = false;
+
+        winsTextShadow.gameObject.SetActive(false);
+        playerMoveCount.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Check if the player finish the movement this round
+        if (player1.GetComponent<PlayerMovement>().moveFinished) {
+            player1.GetComponent<PlayerMovement>().moveAllowed = false;
+            // playerMoveCount.gameObject.SetActive(false);
+            player1.GetComponent<PlayerMovement>().currentWaypointIndex = player1.GetComponent<PlayerMovement>().destinationWaypointIndex;
+
+            // Check for rewarded steps
+            if (!isRewarded) {
+                rewardPlayer();
+            }
+        }
+
+        // // Check if finish the board
+        // if (player1.GetComponent<PlayerMovement>().currentWaypointIndex == player1.GetComponent<PlayerMovement>().waypoints.Length) {
+        //     winsTextShadow.gameObject.SetActive(true);
+        //     playerMoveCount.gameObject.SetActive(false);
+        //     winsTextShadow.GetComponent<Text>().text = "You Wins";
+        //     gameOver = true;
+        // }
+    }
+
+    public static void MovePlayer() {
+        // Reset Reward
+        isRewarded = false;
+
+        // Setup UI
+        playerMoveCount.gameObject.SetActive(true);
+        playerMoveCount.GetComponent<Text>().text = "You move " + diceSideThrown.ToString() + " steps";
+
+        // Setup player movement
+        player1.GetComponent<PlayerMovement>().moveForward = true;
+        player1.GetComponent<PlayerMovement>().destinationWaypointIndex = player1.GetComponent<PlayerMovement>().currentWaypointIndex + diceSideThrown;
+        player1.GetComponent<PlayerMovement>().moveFinished = false;
+        player1.GetComponent<PlayerMovement>().moveAllowed = true;
+    }
+
+    private void rewardPlayer() {
+        // Set rewarded
+        isRewarded = true;
+
+        // Current Index
+        int currentIndex = player1.GetComponent<PlayerMovement>().currentWaypointIndex;
+
+        // Check steps
+        int rewardedSteps = waypoints_reward[currentIndex];
+
+        // Load mini game if no reward and is in minigamesIndexes
+        if (rewardedSteps == 0 && minigamesIndexes.Contains(currentIndex)) {
+            sceneManager.GetComponent<SceneTransitions>().loadScene(sceneName: "HexgonScene");
+            return;
+        }
+
+        // Setup UI
+        playerMoveCount.gameObject.SetActive(true);
+        playerMoveCount.GetComponent<Text>().text = "You are rewarded to move " + rewardedSteps + " steps";
+
+        // Setup player movement
+        if (rewardedSteps > 0) {
+            player1.GetComponent<PlayerMovement>().moveForward = true;
+        } else if (rewardedSteps < 0) {
+            player1.GetComponent<PlayerMovement>().moveForward = false;
+        }
+
+        player1.GetComponent<PlayerMovement>().destinationWaypointIndex = Math.Max(player1.GetComponent<PlayerMovement>().currentWaypointIndex + rewardedSteps, 0);
+        player1.GetComponent<PlayerMovement>().moveFinished = false;
+        player1.GetComponent<PlayerMovement>().moveAllowed = true;
+    }
+}

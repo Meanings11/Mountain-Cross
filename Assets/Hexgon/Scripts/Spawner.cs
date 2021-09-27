0344dd38ac050f38b3975fc82d474daf61754989
Yunxin Chen <tracy.chenyx@gmail.com>
2021-09-26 17:29:01 -0700

    Add hexgon

---
+++ Assets/Hexgon/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+
+    public float spawnRate = 1f;
+    public GameObject hexagonPrefab;
+    public Text pointText;
+
+    private int point = 0;
+    private float nextTimeToSpawn = 0f;
+    private float startTime = 0f;
+
+    private void Start() {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextTimeToSpawn)
+        {
+            if (Time.time >= startTime + 2.5) {
+            point++;
+            pointText.text = point.ToString();
+            }
+            Instantiate(hexagonPrefab);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+}

b094f692fc31878733a342784e0f52d755e2d2de
Mingful <mingful@smu.edu>
2021-09-29 13:42:10 -0700

    change: collison game over

--- Assets/Mosquito/Scripts/Player.cs
+++ Assets/Mosquito/Scripts/Player.cs
@@ -29,6 +29,6 @@ public class Player : MonoBehaviour
     {
         isDead = true;
         hitPlayer.Play();
-        GameManager.instance.GamePause();
+        GameManager.instance.GameOver();
     }
 }

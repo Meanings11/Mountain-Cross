a5a3c06d7b45a7d158dca61de90ee1dbb0e9a611
Yunxin Chen <tracy.chenyx@gmail.com>
2021-09-29 00:55:36 -0700

    Change background for mosquito

--- Assets/Board Scene/Scripts/ScoreManager.cs
+++ Assets/Board Scene/Scripts/ScoreManager.cs
@@ -11,7 +11,7 @@ public class ScoreManager : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-    TotalGameScore.text = totalgamescore.ToString();
+    TotalGameScore.text = "$" + totalgamescore.ToString();
   }
 
   // Update is called once per frame

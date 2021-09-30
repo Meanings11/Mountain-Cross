dff4cdb0381426088ceeeff65d72d163aa5e978d
Yunxin Chen <tracy.chenyx@gmail.com>
2021-09-30 03:15:12 -0700

    Remove unused code

--- Assets/Hexgon/Scripts/Hexagon.cs
+++ Assets/Hexgon/Scripts/Hexagon.cs
@@ -1,32 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine.UI;
-// using UnityEngine.SceneManagement;
-// using System;
 
 public class Hexagon : MonoBehaviour
 {
     public Rigidbody2D rb;
     private float shrinkSpeed = 1f;
 
-    // public Text GameoverText;
-    // public Text TotalPoint;
-    // public Text pointText;
-
-    // public GameObject Canvas;
-    // public GameObject CenterPlayer;
-
-    // public AudioSource hitSource;
-
-    // public bool firstHit = true;
-
     // Start is called before the first frame update
     void Start()
     {
         // hitSource = GetComponent<AudioSource> ();
 
-        rb.rotation = UnityEngine.Random.Range(0f, 360f);
+        rb.rotation = Random.Range(0f, 360f);
         transform.localScale = Vector3.one * 5f;
     }
 
@@ -40,38 +26,4 @@ public class Hexagon : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    // private void OnTriggerEnter2D(Collider2D other)
-    // // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    //     Debug.Log("collide");
-    //     // SceneManager.LoadScene("ReturnScene", LoadSceneMode.Additive);
-    //     Canvas.gameObject.SetActive(false);
-    //     // CenterPlayer.gameObject.SetActive(false);
-    //     hitSource.Play();
-
-    //     if (firstHit == true)
-    //     {
-    //         TotalPoint.text = "Earned $" + pointText.text.ToString() + " in total";
-    //         GameoverText.gameObject.SetActive(true);
-    //         TotalPoint.gameObject.SetActive(true);
-
-    //         // Set score
-    //         int currentGameScore = PlayerPrefs.GetInt("totalGameScore", 0);
-    //         int addedScore = Int16.Parse(pointText.text);
-    //         PlayerPrefs.SetInt("totalGameScore", currentGameScore+addedScore);
-    //     }
-        
-    //     firstHit = false;
-        
-    //     // End scene
-    //     StartCoroutine(LoadEndScene());
-    //     // SceneManager.LoadScene("BoardScene", LoadSceneMode.Single);
-    // }
-
-    // IEnumerator LoadEndScene() {
-    //     yield return new WaitForSeconds(3.5f);
-    //     SceneManager.LoadScene("BoardScene");
-    // }
 }

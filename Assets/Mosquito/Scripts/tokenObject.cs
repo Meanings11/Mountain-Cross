fbd15563815f228b7f3f88713dffb4af863250d7
Mingful <mingful@smu.edu>
2021-10-03 01:05:04 -0700

    add: mos dead effect

--- Assets/Mosquito/Scripts/tokenObject.cs
+++ Assets/Mosquito/Scripts/tokenObject.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class tokenObject : MonoBehaviour
 {
     public float speed;
+    public GameObject splash;
+
+    private Animator _animator;
+    private bool isDead = false;
+    private BoxCollider2D mosCollider;
     
     // Update is called once per frame
 
     
     private void Awake() {
         speed = GameManager.instance.scrollSpeed;
+        _animator = GetComponent<Animator>();
+        mosCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        transform.position += ((Vector3.left * speed) * Time.deltaTime);
+    //    if (!isDead) 
+       transform.position += ((Vector3.left * speed) * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D (Collider2D other) {
         {
             if (other.GetComponent<Player>() != null) {
+                mosCollider.enabled = false;
                 GameManager.instance.BirdScored ();
-                Destroy(gameObject);
+
+                _animator.SetTrigger("mosDie");
+                Destroy(gameObject,0.8f);
+                isDead = true;
+
             }
         }
     }

acf7fd24614941ad4bbf3c0c33e7902b92dce5b5
Xinyu Lu <lawrenceluxinyu@icloud.com>
2021-10-06 01:24:35 -0700

    Fix dialogue bugs and display

--- Assets/Dialogues/Scripts/DialogueManager.cs
+++ Assets/Dialogues/Scripts/DialogueManager.cs
@@ -10,6 +10,8 @@ public class DialogueManager : MonoBehaviour {
     public Text dialogueText;
     public Image character;
 	public AudioSource typeSound;
+    
+	public bool clicked;
 
 	public Queue<Dialogue> dialogues;
 
@@ -18,6 +20,33 @@ public class DialogueManager : MonoBehaviour {
     {
         typeSound = GetComponent<AudioSource> ();
     }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            if (!clicked) {
+                TriggerDialogue();
+                clicked = true;
+            } else {
+                DisplayNextDialogue();
+            }
+        }
+    }
+
+	public void TriggerDialogue() {
+
+		string[] lines = System.IO.File.ReadAllLines("Assets/Dialogues/Resources/Test_Dialogue.txt");
+		dialogues = new Queue<Dialogue>();
+		foreach (string line in lines) {
+			Dialogue currentDialogue = new Dialogue();
+			currentDialogue.name = line.Substring(0, line.IndexOf(':'));
+			currentDialogue.sentence = line.Substring(line.IndexOf(':')+1, line.Length-line.IndexOf(':')-1);
+			currentDialogue.sprite = Resources.Load<Sprite>("Sprites/" + line.Substring(0, line.IndexOf(':')));
+			dialogues.Enqueue(currentDialogue);
+		}
+
+		StartDialogue(dialogues);
+	}
 
     public void StartDialogue(Queue<Dialogue> allDialogue) {
 		dialogues = allDialogue;
